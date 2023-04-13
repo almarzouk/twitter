719ff4d2de3b593e01c671d77f5242852591a5d1
almarzouk <jumaa.almarzouk@gmail.com>
2023-04-14 01:46:26 +0200

    frontned

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://twitter-a6fk.onrender.com/assets/${image}`}
       />
     </Box>
   );
